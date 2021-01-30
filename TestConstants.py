29f990e00937a90484a9fb605b621abe7b8dad9b
Nathaniel Basque-Giroux <nbasquegiroux@gmail.com>
2021-01-30 16:00:33 -0500

    live data for api

--- TestConstants.py
+++ TestConstants.py
@@ -1,8 +1,11 @@
 # Test constants
 
-one = 1
+class venueInfo:
+    venueName = 'Loblaws'
+    venueAddress = '2065A Robertson Rd'
 
-venueName = 'Loblaws'
-venueAddress = '2065A Robertson Rd'
+class BestTimeInfo:
+    Api_Key_Private = 'pri_26aedd82b0a54ffda5e024a9b3a77827'
+    Api_Key_Public = 'pub_fdd2fbb80c5747aea1b10793746d553d'
 
 
--- apiData.py
+++ apiData.py
@@ -1,17 +1,24 @@
 # Processing API Data from https://besttime.app/
 
-
 import requests
 import json
 import TestConstants as con
 
-venue_name = con.venueName
-venue_Address = con.venueAddress
+# Following URL gives a POST for ALL the JSON on the forecast
+# url = "https://besttime.app/api/v1/forecasts"
 
+url = "https://besttime.app/api/v1/forecasts/live"
 
+params = {
+    'api_key_private': con.BestTimeInfo.Api_Key_Private,
+    'venue_name': con.venueInfo.venueName,
+    'venue_address': con.venueInfo.venueAddress
+}
 
+response = requests.request("POST", url, params=params)
+data = json.loads(response.text)
 
-
+print(data)
 
 
 
