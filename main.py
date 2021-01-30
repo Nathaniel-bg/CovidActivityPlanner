f405bff54a3b6c1fca7059e89226396cc828cdb4
DavidSciola <david.sciola1@gmail.com>
2021-01-30 10:58:02 -0500

    testing commit

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print('Hello World')
 
+    print("yo")
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
