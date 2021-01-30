94ff299e86ebe185ecfe53a9a645feb40e44f366
James Couture <jcout071@uottawa.ca>
2021-01-30 14:26:59 -0500

    Adding user input

---
+++ searchAddress.py
@@ -0,0 +1,16 @@
+#imports
+
+#user inputs
+def userInput():
+    #userInfo = [input("Address "), input("City "), input("Province/State "), input("Venue ")]
+    userInfo = ['Potato Dr','Ottawa','Ontario','Superstore']
+    return userInfo
+
+
+
+
+
+#_______________________TEST_HERE___________________________
+test = userInput()
+for i in test:
+    print(i)
