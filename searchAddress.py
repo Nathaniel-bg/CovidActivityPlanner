879bfda848b3073e83662ca43949c7676643d14a
James Couture <jcout071@uottawa.ca>
2021-01-30 15:27:12 -0500

    This file is now my test unit file, dont mind this file

--- searchAddress.py
+++ searchAddress.py
@@ -1,14 +1,14 @@
 #imports
+import requests as rq
+import json
+import tkinter
+import bs4
 
 #user inputs
 def userInput():
     #userInfo = [input("Address "), input("City "), input("Province/State "), input("Venue ")]
     userInfo = ['Potato Dr','Ottawa','Ontario','Superstore']
     return userInfo
-
-
-
-
 
 #_______________________TEST_HERE___________________________
 test = userInput()
